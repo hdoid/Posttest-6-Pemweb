126709f01e55b81ce13362698262d3081e9ecc9e
hdoid <145538419+hdoid@users.noreply.github.com>
2023-10-25 22:23:44 +0800

    posttest6

---
+++ posttest6/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Admin</title>
+    <link rel="stylesheet" href="posttest2style.css">
+</head>
+<body>
+    <div class="form">
+        <img src="asiangames.png.jpg" alt="">
+        <div class="form-container">
+            <h1>Login Admin</h1><hr>
+            <form action="admin.php">
+                <input type="text" name="username" placeholder="Username" class="textfield">
+                <input type="password" name="password" placeholder="Password" class="textfield">
+                <input type="submit" value="Masuk" class="login-btn">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
